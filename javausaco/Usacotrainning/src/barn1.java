88d6f534dcaa006282163ee3a0b7064ce82d9470
Raymond <20248577+javaarchive@users.noreply.github.com>
2018-07-22 14:57:04 -0700

    Add files via upload

---
+++ javausaco/Usacotrainning/src/barn1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class barn1 {
+
+	public static void main(String[] args) throws IOException{
+		
+		// TODO Auto-generated method stub
+		BufferedReader f=new BufferedReader(new FileReader("barn1.in"));
+		StringTokenizer st=new StringTokenizer(f.readLine());
+		int x=Integer.parseInt(st.nextToken());
+		int y=Integer.parseInt(st.nextToken());
+		int z=Integer.parseInt(st.nextToken());
+		int[] stalls=new int[y];
+	}
+
+}
