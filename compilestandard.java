27358db5b1be0f457152eded746ee55e9dfcc679
Raymond <20248577+javaarchive@users.noreply.github.com>
2018-08-26 04:35:11 +0000

    Add files via upload

--- compilestandard.java
+++ compilestandard.java
@@ -1,6 +1,17 @@
 import java.io.*;
+import java.security.MessageDigest;
+import java.security.*;
 public class compilestandard extends usacotools {
-
+	public static String sha256(String input) throws NoSuchAlgorithmException {
+        MessageDigest mDigest = MessageDigest.getInstance("SHA-256");
+        byte[] result = mDigest.digest(input.getBytes());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+         
+        return sb.toString();
+    }
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader f;
@@ -34,6 +45,13 @@ public class compilestandard extends usacotools {
 		}
 	 	pw.println(text);
 	 	pw.close();
+	 	print("utools.java is the one you should copy into your code but usacotools is the one you should extend");
+	 try{
+		 print("utools.java SHA256: "+sha256(text));
+	 	print("usacotools.java SHA256: "+sha256(full));
+	 }catch(Exception e) {
+		 e.printStackTrace();
+	 }
 	 	
 	}
 }
