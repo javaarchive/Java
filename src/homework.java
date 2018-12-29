c165941f199f700e9494cdaf3f9384cae3c47c2c
Raymond <20248577+javaarchive@users.noreply.github.com>
2018-12-29 11:38:02 -0800

    Something...
    I forgot what I added

--- src/homework.java
+++ src/homework.java
@@ -11,10 +11,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
-public class homework {
-	
-
-	
+public class homework{	
 	public static final int LARGE_CONFIG=10001;
 	public static int getsmall(List<Integer> l) {
 		int min=LARGE_CONFIG;
@@ -25,43 +22,46 @@ public class homework {
 		}
 		return min;
 	}
-	public static int grade_assignment(List<Integer> homework) {
+	public static double grade_assignment(List<Integer> homework) {
 		int sum=0;
-		System.out.println("The homework is "+homework);
+		//System.out.println("The homework is "+homework);
 		for(int i=0;i<homework.size();i++) {
 			sum+=homework.get(i);
 		}
 		sum-=getsmall(homework);
-		return sum/(homework.size()-1);
+		return (double) sum/(homework.size()-1);
 		
 	}
 	public static List<Integer >make_bessie_eat_homework(ArrayList<Integer> homework,int k) {
 		//Simulate bessie eating the homework
 		return homework.subList(k, homework.size());
 	}
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws Exception{
+		//$1();
 		// TODO Auto-generated method stub
-		BufferedReader f=new BufferedReader(new FileReader("homework.in"));
+		BufferedReader f=new BufferedReader(new FileReader("10.in"));
 		int N=Integer.parseInt(f.readLine());
 		ArrayList<Integer> grades=new ArrayList<Integer>();
 		StringTokenizer st=new StringTokenizer(f.readLine());
 		for(int i=0;i<N;i++) {
 			grades.add(Integer.parseInt(st.nextToken()));
 		}
-		int bestscore=-1;
-		int score;
+		//System.out.println("homework "+grades);
+		double bestscore=-1;
+		double score;
 		ArrayList<Integer> bestks=new ArrayList<Integer>();
 		for(int i=1;i<(N-1);i++) {
-			System.out.println("K:"+i);
+		//for(int i=N-2;i>0;i--) {
+			//System.out.println("K:"+i);
 			score=grade_assignment(make_bessie_eat_homework(grades,i));
-			System.out.println("Score:"+score);
+			//System.out.println("Score:"+score);
 			if(score>bestscore) {
-				System.out.println("New Score:"+score);
+				//System.out.println("New Score:"+score);
 				bestks.clear();
 				bestks.add(i);
 				bestscore=score;
 			}else if(score==bestscore) {
-				System.out.println("Same score:"+score);
+				//System.out.println("Same score:"+score);
 				bestks.add(i);
 			}
 			
@@ -70,7 +70,11 @@ public class homework {
 		f.close();
 		PrintWriter pw=new PrintWriter(new BufferedWriter(new FileWriter("homework.out")));
 		for(int a:bestks) {pw.println(a);}
+		//pw.println(bestks.get(bestks.size()-1));
 		pw.close();
+		//$1();
+		//System.out.println($r());
+		
 	}
 
 }
