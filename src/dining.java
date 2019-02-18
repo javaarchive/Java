e36ef9adad3fec7817bbb458fa816c68c0f5f19d
Raymond <20248577+javaarchive@users.noreply.github.com>
2019-02-17 21:02:53 -0800

    Workspace broken because of git, how sad.

--- src/dining.java
+++ src/dining.java
@@ -13,7 +13,41 @@ public class dining {
     } 
     static final int OFFSET  = -1; // Offset by 1 for adjancey matirix because index begins at 0
     public static int[] root;
-    
+    private static int[] dijkstra(int[][] graph1, int startVertex) {
+        int N = graph1.length;
+        int[] dists = new int[N];
+        boolean[] added = new boolean[N];
+        for (int vertexIndex = 0; vertexIndex < N; vertexIndex++) {
+            dists[vertexIndex] = Integer.MAX_VALUE;
+            added[vertexIndex] = false;
+        }
+        dists[startVertex] = 0;
+        int[] parents = new int[N];
+        parents[startVertex] = -1;
+        for (int i = 1; i < N; i++) {
+            int nearestVertex = -1;
+            int shortestDistance = Integer.MAX_VALUE;
+            for (int vertexIndex = 0; vertexIndex < N; vertexIndex++) {
+                if (!added[vertexIndex] && dists[vertexIndex] < shortestDistance) {
+                    nearestVertex = vertexIndex;
+                    shortestDistance = dists[vertexIndex];
+                }
+            }
+            added[nearestVertex] = true;
+            for (int vertexIndex = 0; vertexIndex < N; vertexIndex++) {
+                int edgeDistance = graph1[nearestVertex][vertexIndex];
+                if (edgeDistance > 0 && ((shortestDistance + edgeDistance) < dists[vertexIndex])) {
+                    parents[vertexIndex] = nearestVertex;
+                    dists[vertexIndex] = shortestDistance +
+                        edgeDistance;
+                }
+            }
+        }
+        root = parents;
+        return dists;
+        
+
+    }
 	public static void main(String[] args) throws IOException{
 		BufferedReader f = new BufferedReader(new FileReader("dining.in"));
 		StringTokenizer st = new StringTokenizer(f.readLine());
