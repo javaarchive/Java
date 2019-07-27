118c82ae7acfe9481ca506e592623cbc87e8992a
Raymond <20248577+javaarchive@users.noreply.github.com>
2019-07-26 20:44:15 -0700

    Add files via upload

--- Java/src/angry.java
+++ Java/src/angry.java
@@ -6,32 +6,51 @@ import java.util.*;
  */
 public class angry {
 	static List<Integer> field = new ArrayList<Integer>();
-	//public List<Integer> launchSim(int pos){
-	//	
-	//}
+	public static int N;
+	public static int launchSim(int power){
+		int cowsUsed  = 0;
+		int curCow = 0;
+		while(curCow < N) {
+			int pos = field.get(curCow);
+			curCow++;
+			for(int i = pos; i < N; i ++) {
+				if(pos ) {
+					curCow++;
+				}
+			}
+		}
+		return cowsUsed;
+	}
 	public static void main(String[] args) throws IOException{
 		BufferedReader f = new BufferedReader(new FileReader("angry.in"));
 		StringTokenizer st = new StringTokenizer(f.readLine());
-		int N = Integer.parseInt(st.nextToken()); // Number of haybales
+		N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken()); // Number of cows
-		List<Integer> costs = new ArrayList<Integer>(N - 1);
+		//List<Integer> costs = new ArrayList<Integer>(N - 1);
 		for(int i = 0; i < N; i ++) {
 			field.add(Integer.parseInt(f.readLine()));
 		}
 		f.close();
 		field.sort(null);
-		for(int i = 0; i < (N-1); i ++) {
-			costs.add(field.get(i+1) - field.get(i));
-		}
+		int answer = -1;
+		int l = 0;
+		int r = N;
+		while (r - l > 1) {
+			int m = (l + r) / 2;
+			if (launchSim(m) < K) {
+				r = m;
+			} else {
+			            l = m;
+			        }    
+			    }    
+			    
 		System.out.println(field);
-		System.out.println(costs);
+		//System.out.println(costs);
+		
 		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("angry.out")));
-		pw.println(Collections.max(costs));
+		pw.println(answer);
 		pw.close();
 		//System.out.println("");
-		for(int i = 0 ; i < N; i ++) {
-			
-		}
 	}
 
 }
