948577b089cef16255cda0711a7e33fc243c7d4d
Raymond <20248577+javaarchive@users.noreply.github.com>
2019-09-02 10:25:13 -0700

    removed lots of old and unnesscary code

--- dining.java
+++ dining.java
@@ -54,77 +54,29 @@ public class dining {
 			cost.put(new Pair(start-1,end-1), edgeCost);
 		}
 		Map<Pair,Integer> costWithHaybales = new HashMap<>(cost); 
-		//System.out.println("Cost without haybales: "+cost);
 		System.out.println("Graph as an edgelist:  "+graph);
 		int[] taste = new int[N];
-		//Pair key;
 		List<Integer> bales = new ArrayList<>();
-		//Map<Integer, Integer> haybales = new HashMap<>();
-		//cost.get(new Pair(0,1));
 		for(int i = 0; i < K; i ++) {
 			st = new StringTokenizer(f.readLine());
 			int index = Integer.parseInt(st.nextToken()) - 1;
 			bales.add(index);
 			int value = Integer.parseInt(st.nextToken());
-			/*
-			//costWithHaybales.get(new Pair(0,1));
-			for(int node: graph.get(index)) {
-				Pair key = new Pair(index,node);//assert key.equals(new Pair(node,index));
-				//System.out.println("Map: "+costWithHaybales);
-				//System.out.println(key+ " "+costWithHaybales.get(key));
-				//costWithHaybales.put(key, 
-				//		costWithHaybales.get(key) - value);
-			}
-			*/
 			taste[index] = value;
 		}
-		//System.out.println("Cost with haybales:    "+costWithHaybales);
 		f.close();
 		int[] empty = new int[N];
 		Arrays.fill(empty, Integer.MAX_VALUE);
 		empty[N-1] = 0;
-		//distTo = new int[M];
-		//distTo= Arrays.copyOf(empty, N);
 		int[] distOrig,distTo;
-		/*
-		nextNodes.add(N-1);
-		// begin dijkstra from barn with haybales
-		while(visited.size() < N) {
-			System.out.println(visited.size());
-			int u = nextNodes.remove(); // Get next node
-			System.out.println(u);
-			visited.add(u);
-			List<Integer> adj = graph.get(u);
-			for(int node:adj) {
-				if(!visited.contains(node)) {
-					int edgeDist = costWithHaybales.get(new Pair(u,node));
-					int totalDist = distTo[u] + edgeDist;
-					if (totalDist < distTo[node]) 
-	                    distTo[node] = totalDist; 
-					nextNodes.add(node);
-				}
-			}
-		}
-		*/
-		
 		distOrig = dijkstra(cost, graph, N);
-		
-		//for(int bale: graph.get(N-1)) {
-		//	graph.get(bale).remove((Object) (N-1));
-		//}
-		
-		//graph.get(N-1).clear();
-		
 		for(int i = 0 ; i < K;  i++) {
 			int target = bales.get(i);
-			//for(int bale:graph.get(target)) {
-			//	graph.get(bale).remove(target);
-			//}
-			//graph.get(target).clear();
 			graph.get(target).add(N-1);
 			graph.get(N-1).add(target);
 			costWithHaybales.put(new Pair(target,N-1), distOrig[target] - taste[i]);
 		}
+		System.out.println("Modified New Graph  :  "+graph);
 		System.out.println("Cost              :    "+cost);
 		System.out.println("Cost with haybales:    "+costWithHaybales);
 		distTo = dijkstra(costWithHaybales, graph, N);
@@ -167,34 +119,3 @@ class Pair{
 		return Integer.hashCode(this.x) * Integer.hashCode(this.y);
 	}
 }
-// No longer needed
-/*
-class Edge{
-	int x;
-	int y;
-	int weight;
-	public Edge(int x,int y,int weight) {
-		this.x = x;
-		this.y =y;
-		this.weight = weight;
-	}
-	public String toString() {
-		return "("+this.x+"--"+this.y+" = "+this.weight+")";
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if(obj instanceof Edge) {
-			Edge other = (Edge) obj;
-			if((this.x == other.x && this.y == other.y) || (this.x == other.y && this.y == other.x)) {
-				if(this.weight == other.weight) {
-					return true;
-				}else {
-					return false;
-				}
-			}
-			return false;
-		}else {
-			return false;
-		}
-	}
-}*/
